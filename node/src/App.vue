6cd247af8202b90eaad2fa400a595694291e07ee
John Sanders <jws121295@gmail.com>
2019-07-12 20:00:12 -0400

    feat: control form

--- node/src/App.vue
+++ node/src/App.vue
@@ -26,12 +26,82 @@
                   WiFi Settings
                 </v-toolbar-title>
               </v-toolbar>
-              <v-card-text>
-                <v-form>
+              <v-form>
+                <v-card-text>
                   <v-text-field name="ssid" label="WiFi Network" type="text" />
                   <v-text-field name="password" label="Password" type="text" />
-                </v-form>
-              </v-card-text>
+                </v-card-text>
+                <v-toolbar
+                  color="primary"
+                  dark
+                >
+                  <v-toolbar-title>
+                    Device Settings
+                  </v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <p>What should activate the lights?</p>
+                  <v-radio-group v-model="activateMethod" row>
+                    <v-radio
+                      label="Detecting device presence"
+                      value="detect"
+                    />
+                    <v-radio
+                      label="Manual"
+                      value="manual"
+                    />
+                  </v-radio-group>
+                </v-card-text>
+                <v-card-text v-if="activateMethod === 'detect'">
+                  <v-layout wrap>
+                    <v-flex xs12 sm8 pr-2>
+                      <v-text-field name="blueip" label="IP for Blue" type="text" />
+                    </v-flex>
+                    <v-flex xs12 sm4>
+                      <v-btn block color="blue" dark>THIS DEVICE</v-btn>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout wrap>
+                    <v-flex xs12 sm8 pr-2>
+                      <v-text-field name="redip" label="IP for Red" type="text" />
+                    </v-flex>
+                    <v-flex xs12 sm4>
+                      <v-btn block color="red" dark>THIS DEVICE</v-btn>
+                    </v-flex>
+                  </v-layout>
+                </v-card-text>
+                <v-card-text v-if="activateMethod === 'manual'">
+                  <hr>
+                  <v-layout>
+                    <v-flex
+                      xs12
+                      sm6
+                    >
+                      <v-checkbox
+                        v-model="activateBlue"
+                        label="Enable Blue"
+                        color="blue"
+                      />
+                    </v-flex>
+                    <v-flex
+                      xs12
+                      sm6
+                    >
+                      <v-checkbox
+                        v-model="activateRed"
+                        label="Enable Red"
+                        color="red"
+                      />
+                    </v-flex>
+                  </v-layout>
+                </v-card-text>
+                <v-card-text>
+                  <v-slider
+                    v-model="speed"
+                    label="Speed"
+                  />
+                </v-card-text>
+              </v-form>
             </v-card>
           </v-flex>
         </v-layout>
@@ -43,7 +113,14 @@
 <script>
 export default {
   el: '#app',
-  name: 'RootApp'
+  name: 'RootApp',
+  data () {
+    return {
+      activateMethod: 'detect',
+      activateRed: true,
+      activateBlue: true
+    };
+  }
 };
 </script>
 
